a4e0b6fb5ddf325280597c6e6236652d8fc62805
Fernando <fernando60794@hotmail.com>
2019-08-01 07:36:48 -0300

    fmag

---
+++ fmag/bsdiff.h
@@ -0,0 +1,4 @@
+
+extern int createPatch(const char* oldfilename, const char* newfilename, const char* patchfilename);
+
+extern int applyPatch(const char* oldfilename, const char* newfilename, const char* patchfilename);
